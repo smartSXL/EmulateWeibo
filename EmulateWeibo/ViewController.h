b670aecb108bf1648abc87465b6ae59ac3c3f6cc
玲玲 <suxl@shicaidai.com>
2016-03-16 16:13:46 +0800

    Initial Commit

---
+++ EmulateWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmulateWeibo
+//
+//  Created by 001号 on 16/3/16.
+//  Copyright © 2016年 001号. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
